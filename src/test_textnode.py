a146a46fdcf8e6ede419be7055bb587f657a4937
iheartmywife <kylejchermak@gmail.com>
2026-02-05 13:04:17 -0800

    Added functionality to create textnodes from raw markdown strings

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,44 +22,110 @@ class TestTextNode(unittest.TestCase):
 
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
-        html_node = TextNode.text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_bold(self):
         node = TextNode("This is a bold node", TextType.BOLD)
-        html_node = TextNode.text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is a bold node")
 
     def test_italic(self):
         node = TextNode("This is an italic node", TextType.ITALIC)
-        html_node = TextNode.text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "This is an italic node")
 
     def test_code(self):
         node = TextNode("This is a code node in code mode with a load of code...brode", TextType.CODE)
-        html_node = TextNode.text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "This is a code node in code mode with a load of code...brode")
 
     def test_link(self):
         node = TextNode("This is a link node", TextType.LINK, "https://www.boot.dev/lessons/80ddb6c5-8324-4850-a28c-0c6207596857")
-        html_node = TextNode.text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a link node")
         self.assertEqual(html_node.props, {"href": "https://www.boot.dev/lessons/80ddb6c5-8324-4850-a28c-0c6207596857"})
 
     def test_image(self):
         node = TextNode("alt text", TextType.IMAGE, "https://img.url/cat.png")
-        html_node = TextNode.text_node_to_html_node(node)
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
         self.assertEqual(
             html_node.props,
             {"src": "https://img.url/cat.png", "alt": "alt text"},
         )
+
+    def test_markdown_to_textnodes_bold(self):
+        old_nodes = [
+            "This is text with only plain text",
+            "This is text with a **bolded phrase** in the middle",
+            "This is text with only plain text",
+        ]
+        desired_outcome = [
+            TextNode("This is text with only plain text", TextType.TEXT),
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bolded phrase", TextType.BOLD),
+            TextNode(" in the middle", TextType.TEXT),
+            TextNode("This is text with only plain text", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter(old_nodes, "**", TextType.BOLD)
+        self.assertEqual(new_nodes, desired_outcome)
+
+    def test_markdown_to_textnodes_italic(self):
+        old_nodes = [
+            "This is text with only plain text",
+            "This is text with an _italic phrase_ in the middle",
+            "This is text with only plain text",
+        ]
+        desired_outcome = [
+            TextNode("This is text with only plain text", TextType.TEXT),
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("italic phrase", TextType.ITALIC),
+            TextNode(" in the middle", TextType.TEXT),
+            TextNode("This is text with only plain text", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter(old_nodes, "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, desired_outcome)
+
+    def test_markdown_to_textnodes_code(self):
+        old_nodes = [
+            "This is text with only plain text",
+            "This is text with a 'code phrase' in the middle",
+            "This is text with only plain text",
+        ]
+        desired_outcome = [
+            TextNode("This is text with only plain text", TextType.TEXT),
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code phrase", TextType.CODE),
+            TextNode(" in the middle", TextType.TEXT),
+            TextNode("This is text with only plain text", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter(old_nodes, "'", TextType.CODE)
+        self.assertEqual(new_nodes, desired_outcome)
+
+    def test_non_text_textnode(self):
+        old_nodes = [
+            TextNode("bolded phrase", TextType.BOLD),
+            TextNode("italic phrase", TextType.ITALIC),
+            TextNode("code phrase", TextType.CODE),
+            TextNode("This is a text with a **bolded phrase** in the middle", TextType.TEXT),
+        ]
+        desired_outcome = [
+            TextNode("bolded phrase", TextType.BOLD),
+            TextNode("italic phrase", TextType.ITALIC),
+            TextNode("code phrase", TextType.CODE),
+            TextNode("This is a text with a ", TextType.TEXT),
+            TextNode("bolded phrase", TextType.BOLD),
+            TextNode(" in the middle", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter(old_nodes, "**", TextType.BOLD)
+        self.assertEqual(new_nodes, desired_outcome)
 
         """
         TextType(Enum):
--- src/textnode.py
+++ src/textnode.py
@@ -9,6 +9,24 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+# Added this, saw I didnt need it, didnt want to delete it on the off chance I need again
+""" 
+class Delimiters(Enum):
+    BOLD = '**'
+    ITALIC = '_'
+    CODE = "'"
+
+    def match_delimiter(delimiter):
+        match delimiter:
+            case Delimiters.BOLD:
+                return TextType.BOLD
+            case Delimiters.ITALIC:
+                return TextType.ITALIC
+            case Delimiters.CODE:
+                return TextType.CODE
+        raise Exception("Delimiter not found. Please use ** for bold, _ for italic, or ' for code")
+"""
+
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -21,7 +39,7 @@ class TextNode():
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
     
-    def text_node_to_html_node(text_node):
+def text_node_to_html_node(text_node):
         match text_node.text_type:
             case TextType.TEXT:
                 return LeafNode(None, text_node.text)
@@ -35,3 +53,37 @@ class TextNode():
                 return LeafNode("a", text_node.text, {"href": text_node.url})
             case TextType.IMAGE:
                 return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        new_nodes = []
+        for node in old_nodes:
+            if isinstance(node, TextNode):
+                if node.text_type != TextType.TEXT:
+                    new_nodes.append(node)
+                    continue
+                text = node.text
+                original_is_textnode = True
+            else:
+                text = node
+                original_is_textnode = False
+
+            delimiter_count = text.count(delimiter)
+
+            if delimiter_count % 2 == 1:
+                raise Exception(f"Invalid markdown syntax. Delimiter {delimiter} must wrap the desired text")
+
+            if delimiter_count == 0:
+                if original_is_textnode:
+                    new_nodes.append(node)
+                else:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+                continue
+
+            raw_text = text.split(delimiter)
+            for index in range(0, len(raw_text)):
+                if index % 2 == 0 or index == 0:
+                    new_nodes.append(TextNode(raw_text[index], TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(raw_text[index], text_type))
+        return new_nodes
+
