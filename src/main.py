066567756322fa182501c4d0cd3241fc60070167
iheartmywife <kylejchermak@gmail.com>
2026-02-25 07:29:17 -0800

    Added content, added recursive page generation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 from copystatic import copy_contents_from_a_to_b
 import os
 import shutil
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 
@@ -16,6 +18,6 @@ def main():
     print(f"copying contents of {dir_path_static} to {dir_path_public}")
     copy_contents_from_a_to_b(dir_path_static, dir_path_public)
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 main()
